8524a1cc74ae01386d1c9571f7b53e135bbd43ea
zhanghaoran <zhanghaoran91@gmail.com>
2021-05-17 21:41:51 +0800

    都不知为啥突然成功了。

--- src/views/tableShow.vue
+++ src/views/tableShow.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="elTableShow">
     <el-table-show :data="tabData" :option="option" :page.sync="page" searchKey="name" @select-change="selectionChange">
+      <template v-slot:sex="{ row }">
+        <el-input v-model="row.sex"></el-input>
+      </template>
       <template slot="menuLeft">
         <span>送赠品：</span>
         <el-button size="mini" type="primary">添加赠品</el-button>
@@ -15,63 +18,64 @@
 </template>
 
 <script>
-import ElTableShow from '../components/ElTableShow';
+  import ElTableShow from '../components/ElTableShow';
 
-export default {
-  name: 'tableShow',
-  components: { ElTableShow },
-  data() {
-    return {
-      tabData: [],
-      option: {
-        index: true,
-        indexLabel: '',
-        selection: true,
-        align: 'center',
-        menuAlign: 'center',
-        column: [
-          {
-            label: '姓名',
-            prop: 'name',
-          },
-          {
-            label: '性别',
-            prop: 'sex',
-          },
-        ],
-      },
-      page: {
-        currentPage: 2,
-        pageSize: 10,
-        pageSizes: [10, 20, 30, 40, 50, 100],
-        layout: 'total, sizes, prev, pager, next, jumper',
-        background: true,
-      },
-    };
-  },
-  created() {
-    let i = 40;
-    while (i > 0) {
-      this.tabData.push({
-        name: `姓名${i}--${i}`,
-        sex: i % 2,
-        id: i,
-      });
-      i--;
-    }
-  },
-  methods: {
-    selectionChange(list) {
-      console.log(list);
+  export default {
+    name: 'tableShow',
+    components: { ElTableShow },
+    data() {
+      return {
+        tabData: [],
+        option: {
+          index: true,
+          indexLabel: '',
+          selection: true,
+          align: 'center',
+          menuAlign: 'center',
+          column: [
+            {
+              label: '姓名',
+              prop: 'name',
+            },
+            {
+              label: '性别',
+              prop: 'sex',
+              slot: true,
+            },
+          ],
+        },
+        page: {
+          currentPage: 2,
+          pageSize: 10,
+          pageSizes: [10, 20, 30, 40, 50, 100],
+          layout: 'total, sizes, prev, pager, next, jumper',
+          background: true,
+        },
+      };
     },
-    handleDel(row) {
-      console.log(row);
+    created() {
+      let i = 40;
+      while (i > 0) {
+        this.tabData.push({
+          name: `姓名${i}--${i}`,
+          sex: i % 2,
+          id: i,
+        });
+        i--;
+      }
     },
-  },
-};
+    methods: {
+      selectionChange(list) {
+        console.log(list);
+      },
+      handleDel(row) {
+        console.log(row);
+      },
+    },
+  };
 </script>
-<style scoped lang='scss'>
-.elTableShow {
-  font-size: 14px;
-}
+<style scoped lang="scss">
+  .elTableShow {
+    font-size: 14px;
+  }
 </style>
